d90cd082c70979962dce5bc9236dbe8a9bc02dd3
Seraphinae <gustavejulien@laposte.net>
2018-02-06 11:30:36 +0100

    add strings.c & test push

---
+++ strings.c
@@ -0,0 +1,18 @@
+#include "proto.h"
+
+void	my_putchar(char c)
+{
+  write(1, &c, 1);
+}
+
+void	my_putstr(char *str)
+{
+  int	i;
+
+  i = 0;
+  while (str[i] != '\0')
+    {
+      putchar(str[i]);
+      i++;
+    }
+}
